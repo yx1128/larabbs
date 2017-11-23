e8a772c904b07a0b487196ecc0db8611967b06fd
dmnyx <1262937469@qq.com>
2017-11-23 05:20:53 +0000

    Basic user profile editing

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,4 @@ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
-Route::get('/users/{user}', 'UsersController@show')->name('users.show');
-Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
-Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
