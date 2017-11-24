e4c6d28faebe247ee73be29e8d5b9e1505cfe78a
dmnyx <1262937469@qq.com>
2017-11-24 07:17:55 +0000

    Notify user in email with Queue supported

--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -22,7 +22,8 @@ class TopicReplied extends Notification
 
     public function via($notifiable)
     {
-        // 开启通知的频道
+      // 开启通知的频道
+        //return ['database', 'mail'];
         return ['database'];
     }
 
@@ -42,5 +43,14 @@ class TopicReplied extends Notification
             'topic_id' => $topic->id,
             'topic_title' => $topic->title,
         ];
+    }
+
+    public function toMail($notifiable)
+    {
+        $url = $this->reply->topic->link(['#reply' . $this->reply->id]);
+
+        return (new MailMessage)
+                    ->line('你的话题有新回复！')
+                    ->action('查看回复', $url);
     }
 }
