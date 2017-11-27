e245b8e960c86ace076b330eaa24080beeadd3c7
dmnyx <1262937469@qq.com>
2017-11-27 07:34:22 +0000

    Integrate laravel-permission package

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,10 +5,17 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Auth;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use Notifiable;
+
+    use HasRoles;
+
+    use Notifiable {
+        notify as protected laravelNotify;
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -43,10 +50,7 @@ class User extends Authenticatable
        return $this->hasMany(Reply::class);
    }
 
-   use Notifiable {
-       notify as protected laravelNotify;
-   }
-   public function notify($instance)
+    public function notify($instance)
    {
        // 如果要通知的人是当前用户，就不必通知了！
        if ($this->id == Auth::id()) {
